79447e6a6303d84b1bc9bf51ae79a960c0692983
AbekawaMochiko <shandy.gaff.87@gmail.com>
2025-03-24 13:04:23 +0000

    [Finish]modelファイルにアソシエーションの記載

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :users, onry: [new, :show, :edit]
+  get 'top' => 'homes#top'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
