a79f5aed66570ff00fecd28239a867c2734a6303
AbekawaMochiko <shandy.gaff.87@gmail.com>
2025-03-25 13:12:16 +0000

    [Finish]viewファイル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, onry: [new, :show, :edit]
+  resources :users, onry: [:show, :edit]
   get 'top' => 'homes#top'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
