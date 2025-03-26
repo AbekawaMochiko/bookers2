5b41945e19deb5becd4ffa048e368915678b6af9
AbekawaMochiko <shandy.gaff.87@gmail.com>
2025-03-26 12:02:02 +0000

    [start] headerに必要なリンクを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users, onry: [:show, :edit]
   get 'top' => 'homes#top'
+  get '/about', to: 'pages#about'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
