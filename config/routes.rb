3605c00abc085838d68f0593320d2f13887eccab
AbekawaMochiko <shandy.gaff.87@gmail.com>
2025-04-14 05:25:35 +0000

    [add] sign_upページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :users, onry: [:show, :edit]
+  resources :users, onry: [:show, :edit, :new, :sign_up]
   get 'top' => 'homes#top'
-  get '/about', to: 'pages#about'
+  get 'home/about', to: 'homes#about'
+  get 'users/sign_up', to: 'users#sign_up'
   devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
