72c5617a8619225a6b459ffeb23c980d4c9dec15
AbekawaMochiko <shandy.gaff.87@gmail.com>
2025-04-26 05:17:00 +0000

    [Fix] 名前でログインできるように設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :users, onry: [:show, :edit, :new, :sign_up]
+  devise_for :users
+  resources :users, onry: [:show, :edit, :new,]
   get 'top' => 'homes#top'
   get 'home/about', to: 'homes#about'
-  get 'users/sign_up', to: 'users#sign_up'
-  devise_for :users
+  
+  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
