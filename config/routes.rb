6b2babf6ed6e9c6920ff95ee4e7d71163c681e60
AbekawaMochiko <shandy.gaff.87@gmail.com>
2025-05-12 22:33:49 +0000

    [Fix] showページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, onry: [:show, :edit, :new,]
+  resources :users, only: [:show, :edit, :new, :index]
   get 'top' => 'homes#top'
   get 'home/about', to: 'homes#about'
-  
+  resources :books, only: [:show, :edit, :index, :create, :update, :destroy]
   
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
